d01c211b0aebd900dd2a85c482c4f2f0e6c9351c
RusherPan <rusherpan@gmail.com>
2021-12-08 07:11:30 +0800

    更新tesla

--- learnopengl/tesla/cube.fs
+++ learnopengl/tesla/cube.fs
@@ -4,9 +4,20 @@ out vec4 FragColor;
 in vec4 fColor;
 in vec2 Pos;
 
+uniform float iTime;
+
+#define PI 3.1415926
+
 void main()
 {
 //     FragColor = vec4(1.0,0.0,0.0,0.4);
 //     FragColor = fColor;
     FragColor = vec4(fColor.xyz,0.3);
+
+//          vec2 iResolution = vec2(800,600);
+//     	    vec2 uv = gl_FragCoord.xy / iResolution.xy;
+//          vec2 pos = PI *(uv*2.-1.);
+//          vec4 color =vec4(0.2, 0.6, 1., 1.)* abs(sin(20.*pos.y + 20.*sin(pos.x + iTime)));
+//          FragColor = color;
+
 }
--- learnopengl/tesla/cube.vs
+++ learnopengl/tesla/cube.vs
@@ -8,10 +8,14 @@ out vec2 Pos;
 uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
+uniform float iTime;
+
 
 void main()
 {
-    gl_Position =  projection * view * model * vec4(aPos, 0.0, 1.0);
+    // vec4 pos = vec4(aPos.x,aPos.y,sin(iTime + aPos.y),1.0);
+    vec4 pos = vec4(aPos.xy,0.0,1.0);
+    gl_Position =  projection * view * model * pos;
     fColor = aColor;
     Pos = aPos;
 }
